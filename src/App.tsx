15a64c500a801574c470072a93e5d550ac59b76c
Arnav Singh <108.arnav@gmail.com>
2025-02-15 16:57:57 -0700

    feat: added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from "./home";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       {Home()}
+      <Analytics />
     </ThemeProvider>
   );
 }
